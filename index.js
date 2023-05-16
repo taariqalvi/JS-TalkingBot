3b15caf3fbfe8c4d9012a2a55a1206f6c18c8c19
Tariq Alvi <91257843+taariqalvi@users.noreply.github.com>
2023-05-16 06:44:54 +0500

    changes in questions

--- index.js
+++ index.js
@@ -49,41 +49,41 @@ function computerSpeech(words) {
 // Built-in questions / answers function:
 function determineWords(cmpSpeech, words) {
 
-    if (words.includes("how are you")) {
+    if (words.includes("How are you")) {
         cmpSpeech.text = "I am fine, thank you, this is really very nice to meet you";
     }
-    if (words.includes("who am I")) {
+    if (words.includes("Who am I")) {
         cmpSpeech.text = "You are my master, sir, and your name is taariq aalvee";
     }
     if (words.includes("who are you")) {
         cmpSpeech.text = "I am a simple robot, and your assisstant, sir";
     }
-    if (words.includes("how is the weather today")) {
-        cmpSpeech.text = "the weather looks fine today, for more details please link me with some weather API and I will serve you the best results";
+    if (words.includes("How is the weather today")) {
+        cmpSpeech.text = "The weather looks fine today, for more details please link me with some weather API and I will serve you the best results";
     }
-    if (words.includes("do you like me")) {
+    if (words.includes("Do you like me")) {
         cmpSpeech.text = "I ecactly don't know if you're my master or not, even then, yes, i like you, and i like your voice, and i like the way you are talking";
     }
-    if (words.includes("do you love me")) {
-        cmpSpeech.text = "off course i love you, love is in the air, and love is everywhere";
+    if (words.includes("Do you love me")) {
+        cmpSpeech.text = "Off course i love you, love is in the air, and love is everywhere";
     }
-    if (words.includes("are you a poet")) {
-        cmpSpeech.text = "would that I were, but I am not, I am just using some intellisense for your service";
+    if (words.includes("Are you a poet")) {
+        cmpSpeech.text = "Would that I were, but I am not, I am just using some intellisense for your service";
     }
-    if (words.includes("open the Google")) {
-        cmpSpeech.text = "You forget to say, please. Sir.";
+    if (words.includes("Open the Google")) {
+        cmpSpeech.text = "You forget to say please. Sir.";
     }
-    if (words.includes("open the Google please")) {
-        cmpSpeech.text = "opening Google for you. Sir.";
+    if (words.includes("Open the Google please")) {
+        cmpSpeech.text = "Opening Google for you. Sir.";
         window.open("https://www.google.com/");
     }
-    if (words.includes("what is your name")) {
-        cmpSpeech.text = "my name is Smentha, and I am an AI based robotic voice, just to facilitate you at my level best, but my capabilities are limited yet";
+    if (words.includes("What is your name")) {
+        cmpSpeech.text = "My name is Smentha, and I am an AI based robotic voice, just to facilitate you at my level best, but my capabilities are limited yet";
     }
-    if (words.includes("what is love")) {
-        cmpSpeech.text = "love is the most beautiful phenomenon on the planet, it gives us reasons to live, and gives a hope too that why should we live";
+    if (words.includes("What is love")) {
+        cmpSpeech.text = "Love is the most beautiful phenomenon on the planet, it gives us reasons to live, and gives a hope too that why should we live";
     }
-    if (words.includes("read me some poem")) {
+    if (words.includes("Read me some poem")) {
         cmpSpeech.text = "Ok sir please listen carefully. Tonight is the night when one says I'm sorry. Tonight is the night when the other has to say, so goes the story";
     }
 }
